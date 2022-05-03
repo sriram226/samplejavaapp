e5d65b1dd0e5d3b77c73d15c49bf75e579e3a8f7
sriram226 <102042947+sriram226@users.noreply.github.com>
2022-05-03 08:53:47 +0530

    Update changetag.sh

--- changetag.sh
+++ changetag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed "s/tagVersion/$1/g" deploy/sampleapp-deploy-k8s.yml > deploy/sampleapp-deploy-k8.yml
+sed "s/bno/$1/g" deploy/sampleapp-deploy-k8s.yml > deploy/sampleapp-deploy-k8.yml
